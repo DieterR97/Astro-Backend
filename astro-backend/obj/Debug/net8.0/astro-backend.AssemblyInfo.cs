d53aedb2f63909f6bd219363519dbc677f7d33ca
DieterR97 <221122@virtualwindow.co.za>
2024-08-13 23:31:37 +0200

    changed EntityFrameworkCore versions all to 8.0.8 in astro-backend.csproj

--- astro-backend/obj/Debug/net8.0/astro-backend.AssemblyInfo.cs
+++ astro-backend/obj/Debug/net8.0/astro-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("astro-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efce0feb99cefe67e1b9ee6688f747d452e1a20d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+112e74959e3897ac6226c7e25d1340b16333ab1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("astro-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("astro-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
